2280bd3d24eab606d27769f8c347c8ccc066b4e3
veronicazapata <veritozapata12@gmail.com>
2016-03-04 16:58:44 -0500

    consulta de horarios de las oficinas

--- practice/WebContent/HorariosOficinas.jsp
+++ practice/WebContent/HorariosOficinas.jsp
@@ -1,12 +1,30 @@
+<%@page import="java.sql.ResultSet"%>
+<%@page import="java.sql.DriverManager"%>
+<%@page import="java.sql.Statement"%>
+<%@page import="java.sql.Connection"%>
+<%@page import="BD.ConsultaOficinas"%>
+<%@page import="java.util.ArrayList"%>
+<%@page import="Clases.Oficinas"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+<title>Aplicacion UCOmaps</title>
 </head>
 <body>
+<h1>Consulte los horarios de las oficinas</h1>
 
-</body>
+	<br>oficina:<select name="oficina"> 
+                    <%ArrayList<Oficinas> lista = ConsultaOficinas.getOficinas();
+                        for (Oficinas h : lista) {
+                    %>
+                    
+                    <option value="<%=h.getOfi_nombre()%>"><%=(h.getOfi_nombre()+" Hora Apertura: "+ h.getOfi_hora_apertura()+ " Hora Cierre: "+ h.getOfi_hora_cierre())%></option>
+                    
+                    <%}
+                    %>
+                </select><br> 
+                </body>
 </html>
---
+++ practice/src/BD/ConsultaOficinas.java
@@ -0,0 +1,35 @@
+package BD;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+
+
+
+import Clases.Oficinas;
+import Clases.Salas;
+public class ConsultaOficinas {
+	@SuppressWarnings("finally")
+	public static ArrayList<Oficinas> getOficinas() {
+        ArrayList<Oficinas> lista = new ArrayList<Oficinas>();
+        Connection con = null;
+        Statement sql = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ucomaps", "root", "");
+            sql = con.createStatement();
+            ResultSet rs = sql.executeQuery("Select  * from oficinas");
+            while (rs.next()) {
+                Oficinas h = new Oficinas(rs.getString("ofi_bloque"), rs.getString("ofi_nombre"), rs.getInt("ofi_telefono"), rs.getString("ofi_horario_apertura"), rs.getString("ofi_horario_cierre"), rs.getString("ofi_descripcion"));
+                lista.add(h);
+            }
+            System.out.println("conexion establecida ");
+        } catch (Exception e) {
+            System.out.println("error en la conexion" + e);
+        } finally {
+            return lista;
+        }
+}
+}
